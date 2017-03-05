3309d783d9f68ab6ac4863dbb0390d965f9f7489
Jason Smith <smija13@gmail.com>
2017-03-04 23:24:05 -0800

    Evening out the battle system.

--- TheHiddenPotato/TheHiddenPotato/Enemies.cs
+++ TheHiddenPotato/TheHiddenPotato/Enemies.cs
@@ -22,18 +22,7 @@ namespace theHiddenPotato
             strength = _strength;
             name = "Giant Potato Troll";
         }
-    }
-
-    public class spudBaby : Enemy
-    {
-        public spudBaby(int _level, int _health, int _strength)
-        {
-            level = _level;
-            health = _health;
-            strength = _strength;
-            name = "Spud Baby";
-        }
-    }
+    }    
 
     public class yamBeast : Enemy
     {
@@ -42,16 +31,7 @@ namespace theHiddenPotato
             level = _level;
             health = _health;
             strength = _strength;
-        }
-    }
-
-    public class totArmy : Enemy
-    {
-        public totArmy(int _level, int _health, int _strength)
-        {
-            level = _level;
-            health = _health;
-            strength = _strength;
+            name = "Yam Beast";
         }
     }
 
@@ -62,6 +42,29 @@ namespace theHiddenPotato
             level = _level;
             health = _health;
             strength = _strength;
+            name = "Papas";
+        }
+    }
+
+    public class totArmy : Enemy
+    {
+        public totArmy(int _level, int _health, int _strength)
+        {
+            level = _level;
+            health = _health;
+            strength = _strength;
+            name = "Tot Army";
+        }
+    } 
+
+    public class spudBaby : Enemy
+    {
+        public spudBaby(int _level, int _health, int _strength)
+        {
+            level = _level;
+            health = _health;
+            strength = _strength;
+            name = "Spud Baby";
         }
     }
 }
--- TheHiddenPotato/TheHiddenPotato/Program.cs
+++ TheHiddenPotato/TheHiddenPotato/Program.cs
@@ -11,123 +11,15 @@ namespace theHiddenPotato
 
 
         public static int playerHealth = 200;
-        public static int playerAttack = 20;
+        public static int playerAttack = 30;
         public static bool playerFight = false;
 
+
+        // Custom roll function
         public static int Roll(int _start, int _end)
         {
             Random rnd = new Random();
             return rnd.Next(_start, _end);
-        }
-
-        // Determines if you encounter an enemy or not
-        public static void ChanceOfEncounter(int _fnum, int _lNum)
-        {
-            int roll;
-            int level;
-            int health;
-            int attack;
-
-
-            roll = Roll(_fnum, _lNum);
-
-            switch (roll)
-            {
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                    break;
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
-                case 10:
-                    level = Roll(5, 11);
-                    health = level * 20;
-                    attack = level * 4;
-
-
-                    GiantPotatoTroll enemy1 = new GiantPotatoTroll(level, health, attack);
-                    battle(enemy1.name, enemy1.health, enemy1.strength);
-                    break;
-                case 11:
-                case 12:
-                    level = Roll(1, 5);
-                    health = level * 2;
-                    attack = level * 1;
-
-                    spudBaby enemy2 = new spudBaby(level, health, attack);
-                    battle(enemy2.name, enemy2.health, enemy2.strength);
-                    break;
-            }
-        }
-
-        public static void battle(string _enemy, int _health, int _attack)
-        {
-            playerFight = true;
-
-            Console.WriteLine();
-            Console.WriteLine("You have entered battle with a " + _enemy);
-            Console.WriteLine("The enemies health is: " + _health);
-            Console.WriteLine();
-
-            int enemyHealth = _health;
-            int enemyAttack = _attack;
-            int flip = Roll(1, 3);
-
-            while (playerFight)
-            {
-                if (enemyHealth > 0)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Would you like to attack?");
-                    string response = Console.ReadLine();
-
-                    if (response.ToLower() == "yes")
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("You attacked the creature.");
-                        enemyHealth -= playerAttack;
-
-                        if (flip == 1)
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("You were struck by the enemy.");
-                            playerHealth -= enemyAttack;
-                            Console.WriteLine("Your health is now: " + playerHealth);
-                        }
-
-
-
-                        if (enemyHealth > 0)
-                        {
-                            Console.WriteLine("The enemy health is now: " + enemyHealth);
-                        }
-                        else if (enemyHealth <= 0)
-                        {
-                            Console.WriteLine("You beat the enemy!!");
-                        }
-
-                    }
-                    else if (response.ToLower() == "no")
-                    {
-                        Console.WriteLine("There is no running.");
-                        continue;
-                    }
-                    else if (response.ToLower() != "yes" && response.ToLower() != "no")
-                    {
-                        Console.WriteLine("Please enter YES or NO.");
-                    }
-                }
-                else if (enemyHealth <= 0)
-                {
-                    break;
-                }
-
-            }
-
         }
 
         // main game loop
@@ -148,7 +40,7 @@ namespace theHiddenPotato
 
                 if (choice.ToLower() == "forward")
                 {
-                    ChanceOfEncounter(1, 13);
+                    ChanceOfEncounter(1, 101);
                 }
                 else if (choice.ToLower() == "left")
                 {
@@ -166,7 +58,196 @@ namespace theHiddenPotato
                 {
                     break;
                 }
+
+                if (playerHealth <= 0)
+                {
+                    Console.WriteLine("Press any key to exit the game.");
+                    Console.ReadKey();
+                }
             }
         }
+
+
+        // Determines if you encounter an enemy or not
+        public static void ChanceOfEncounter(int _fnum, int _lNum)
+        {
+            int roll;
+            int level;
+            int health;
+            int attack;
+            int monsterSelect;
+
+
+            roll = Roll(_fnum, _lNum);
+
+            if (roll > 75)
+            {
+                monsterSelect = Roll(_fnum, _lNum);
+
+                if (monsterSelect > 99)
+                {
+                    level = Roll(5, 11);
+                    health = level * 20;
+                    attack = level * 4;
+
+
+                    GiantPotatoTroll enemy1 = new GiantPotatoTroll(level, health, attack);
+                    battle(enemy1.name, enemy1.health, enemy1.strength);
+                }
+                else if (monsterSelect > 60)
+                {
+                    level = Roll(5, 11);
+                    health = level * 20;
+                    attack = level * 4;
+
+
+                    yamBeast enemy2 = new yamBeast(level, health, attack);
+                    battle(enemy2.name, enemy2.health, enemy2.strength);
+                }
+                else if (monsterSelect > 50)
+                {
+                    level = Roll(5, 11);
+                    health = level * 20;
+                    attack = level * 4;
+
+
+                    Papas enemy3 = new Papas(level, health, attack);
+                    battle(enemy3.name, enemy3.health, enemy3.strength);
+                }
+                else if (monsterSelect > 30)
+                {
+                    level = Roll(2, 9);
+                    health = level * 4;
+                    attack = level * 2;
+
+
+                    totArmy enemy4 = new totArmy(level, health, attack);
+                    battle(enemy4.name, enemy4.health, enemy4.strength);
+                }
+                else if (monsterSelect > 8)
+                {
+                    level = Roll(1, 5);
+                    health = level * 2;
+                    attack = level * 1;
+
+                    spudBaby enemy5 = new spudBaby(level, health, attack);
+                    battle(enemy5.name, enemy5.health, enemy5.strength);
+                }
+                
+            }
+        }
+
+
+        /// <summary>
+        /// Battle loop runs here.
+        /// </summary>
+        /// <param name="_enemy"></param>
+        /// <param name="_health"></param>
+        /// <param name="_attack"></param>
+        public static void battle(string _enemy, int _health, int _attack)
+        {
+            playerFight = true;
+
+            Console.WriteLine();
+            Console.WriteLine("You have entered battle with a " + _enemy);
+            Console.WriteLine();
+
+            int enemyHealth = _health;
+            int enemyAttack = _attack;
+
+            while (playerFight)
+            {
+                
+
+                if (enemyHealth > 0)
+                {
+
+                    int playerFlip = Roll(1, 101);
+                    int enemyFlip = Roll(1, 101);
+
+
+                    Console.WriteLine();
+                    Console.WriteLine("Would you like to attack?");
+                    string response = Console.ReadLine();
+                    Console.WriteLine("-------------------------------------------");
+
+                    // Fighting happens here
+                    #region Actual fight
+                    if (response.ToLower() == "yes")
+                    {                      
+
+                        if (playerFlip < 50)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("You attacked the creature.");
+                            enemyHealth -= playerAttack;
+                        }
+                        else if (playerFlip >= 50)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("The enemy blocked your attack.");
+                        }
+                        
+
+                        // Enemy Strikes player
+                        if (enemyFlip >= 70)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("You were struck by the enemy.");
+                            playerHealth -= enemyAttack;
+                            Console.WriteLine("Your health is now: " + playerHealth);
+                            Console.WriteLine("-------------------------------------------");
+
+                            if (playerHealth <= 0)
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("The Enemy has killed you.");
+                                Console.WriteLine("Your adventure is now over.");
+                                Console.WriteLine();
+                                Console.WriteLine("-------------------------------------------");
+                                Console.WriteLine();
+                                break;
+                            }
+                        }
+                        else if (enemyFlip < 70)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("The enemy has missed.");
+                        }
+
+                        // If enemy is dead
+                        if (enemyHealth <= 0)
+                        {
+                            Console.WriteLine("You beat the enemy!!");
+                            Console.WriteLine("-------------------------------------------");
+                        }
+
+                        
+
+                    }
+                    #endregion
+
+                    // Check for other input
+                    #region Other Responses
+                    else if (response.ToLower() == "no")
+                    {
+                        Console.WriteLine("There is no running.");
+                        continue;
+                    }
+                    else if (response.ToLower() != "yes" && response.ToLower() != "no")
+                    {
+                        Console.WriteLine("Please enter YES or NO.");
+                    }
+                    #endregion
+                }
+                else if (enemyHealth <= 0)
+                {
+                    break;
+                }
+
+            }
+
+        }
+
     }
 }
