cb7a779a2e0886990b5db11e61bb32ac58dc3303
Jason Smith <smija13@gmail.com>
2017-11-13 22:03:59 -0800

    Changed the dotnet version

---
+++ TheHiddenPotato/TheHiddenPotato/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHiddenPotato
+{
+    public class Item
+    {
+        public string itemName;
+        public string itemType;
+        public int itemPower;
+
+    }
+
+    public class HealthPotion : Item
+    {
+        public HealthPotion(string _type, int _power)
+        {
+            itemType = _type;
+            itemName = "Health Potion";
+        }
+    }
+}
